378ec33043863bd70cbcde2a22aea8dbd3ff860d
iwakiriK <balssearch0113@gmail.com>
2018-10-11 14:24:01 +0900

    Update 0を10進数に統合し1行削減

--- Lexer.java
+++ Lexer.java
@@ -4,18 +4,17 @@ public class Lexer {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         String[] type = {
-			"0",
-			"^[1-9][0-9]*",
+			"^(0|([1-9][0-9]*))",
 			"^[0-9]*[a-fA-F][0-9a-fA-F]*",
             "^0[xX][0-9a-fA-F]+",
             "^(0|[1-9][0-9]*)\\.[0-9]*",
             "^\\.[0-9]+"
         };
         String input = scan.next(), ans = "ERR";
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             Pattern p = Pattern.compile(type[i]);
             Matcher m = p.matcher(input);
-            if (m.find()) ans = (i < 4 ? "INT" : "DEC") + m.group();
+            if (m.find()) ans = (i < 3 ? "INT" : "DEC") + m.group();
         }
         System.out.println(ans);
     }
